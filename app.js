2e37c65a68c6014e7c55773d395a780dd4f9df2e
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-05 10:28:28 +0200

    Add middlewares

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import params from 'strong-params';
 import routes from './config/routes';
 import db from './config/database';
 
@@ -7,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(params.expressMiddleware());
 app.use('/api', routes);
 
 app.listen(port, () => {
