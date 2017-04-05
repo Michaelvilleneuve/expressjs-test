95214f3030b8ea372f4f97d35afc61db1acdc3a4
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-05 10:28:14 +0200

    Improved user service

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  fid_number: String
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minlength: [6, 'Password must be at least 6 chars long']
+  },
+  fid_number: {
+    type: String,
+    required: true
+  }
 });
 
 const User = mongoose.model('User', schema);
--- api/services/users/users.js
+++ api/services/users/users.js
@@ -1,9 +1,33 @@
 import User from '../../../api/models/user';
 
 const Users = {
+
   index(req, res) {
     User.find({}, (err, users) => res.json(users));
-  }
+  },
+
+  show(req, res) {
+    User.find({ _id: req.params.id })
+        .then((user) => res.json(user))
+        .catch(() => res.sendStatus(404));
+  },
+
+  create(req, res) {
+    const user = new User(this.params(req));
+    user.save()
+        .then(() => res.json(user))
+        .catch((err) => res.json(err));
+  },
+
+  destroy(req, res) {
+    User.remove({ _id: req.params.id })
+        .then(() => res.sendStatus(200))
+        .catch(() => res.sendStatus(404));
+  },
+
+  params(req) {
+    return req.parameters.permit('email', 'name', 'password', 'fid_number').value();
+  },
 };
 
 export default Users;
