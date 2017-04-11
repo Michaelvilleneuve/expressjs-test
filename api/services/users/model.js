e1c7b74995916fb4fabed6524fdc18446394ebf4
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-11 10:45:12 +0200

    Improved user model with journey specific code

--- api/services/users/model.js
+++ api/services/users/model.js
@@ -2,29 +2,20 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import sha256 from 'sha256';
 import config from '../../../config/config';
+import Journey from '../journeys/model';
 
 const SALT_WORK_FACTOR = 10;
 const schema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: {
     type: String,
     required: true,
     select: false,
     minlength: [6, 'Password must be at least 6 chars long']
   },
-  fid_number: {
-    type: String,
-    required: true
-  },
-  token: String
+  fid_number: { type: String, required: true },
+  token: String,
 });
 
 schema.pre('save', function (next) {
@@ -38,6 +29,11 @@ schema.pre('save', function (next) {
       next();
     });
   });
+});
+
+// eslint-disable-next-line
+schema.post('remove', function (user) {
+  Journey.remove({ user }).exec();
 });
 
 schema.statics.authenticate = function (email, password) {
@@ -63,6 +59,10 @@ schema.methods.comparePassword = function (password) {
 schema.methods.generateToken = function () {
   this.token = sha256(`${this.email}${this.password}${config.secret}`);
   return this;
+};
+
+schema.methods.journeys = function () {
+  return Journey.find({ user: this });
 };
 
 const User = mongoose.model('User', schema);
